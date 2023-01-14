fae4729a1db6c8e4e4d72771477d09886cd4b039
economy0 <eddisonjr@swu.ac.kr>
2023-01-15 01:15:06 +0900

    upload

---
+++ quadrant.py
@@ -0,0 +1,14 @@
+# 14681
+# 사분면 고르기
+
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:  # x, y: 양수
+    print("1")
+elif x < 0 and y > 0:  # x: 음수, y: 양수
+    print("2")
+elif x < 0 and y < 0:  # x, y: 음수
+    print("3")
+else:  # x: 양수, y: 음수
+    print("4")
